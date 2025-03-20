f1a6b59179592426856b2c6799761bf494bb5146
kris-985 <kris.bakalov98@gmail.com>
2025-03-20 03:21:26 +0200

    Refactor ScrollToTop component to remove unnecessary React import

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import styled, { keyframes } from "styled-components";
 import { FaArrowUp } from "react-icons/fa";
 import { scrollToTop } from "../utils/helpers";
